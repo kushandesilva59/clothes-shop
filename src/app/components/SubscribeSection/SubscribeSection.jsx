1d59aa833b9b0b90d136599440d2c6dd8510960e
kushan de silva <kushandesilva801@gmail.com>
2025-05-18 00:29:40 +0530

    update

--- src/app/components/SubscribeSection/SubscribeSection.jsx
+++ src/app/components/SubscribeSection/SubscribeSection.jsx
@@ -1,14 +1,11 @@
-import React from 'react'
+import React from "react";
 import { Inter, Lato } from "next/font/google";
-import styles from '../../styles/SubscribeSection.module.css'
+import styles from "../../styles/SubscribeSection.module.css";
 
 const inter = Inter({
   subsets: ["latin"], // Optional: Choose subsets
   weight: ["400", "500", "700"], // Optional: Specify desired font weights
 });
-
-
-
 
 const lato = Lato({
   subsets: ["latin"],
@@ -21,7 +18,7 @@ const SubscribeSection = () => {
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4 relative">
       SubscribeSection
       <div className="flex flex-col">
-        <h4 className={`text-center ${styles.title}`}>
+        <h4 className={`text-center ${styles.title} ${lato.className}`}>
           Subscribe to our newsletter to get updates to our latest collections
         </h4>
 
@@ -29,7 +26,7 @@ const SubscribeSection = () => {
           Get 20% off on your first order just by subscribing to our newsletter
         </h6>
 
-        <div className='flex justify-between w-1/2'>
+        <div className="flex justify-between w-1/2">
           <div className={styles.inputWrapper}>
             <span className={styles.icon}>📧</span>
             <input
@@ -48,6 +45,6 @@ const SubscribeSection = () => {
       </div>
     </div>
   );
-}
+};
 
-export default SubscribeSection
+export default SubscribeSection;
